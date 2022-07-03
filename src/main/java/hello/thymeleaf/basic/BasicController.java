074b28ece83a59871faceb7eeb3a4ffcc6cffe64
Jeewoo <cheltus11@gmail.com>
2022-07-03 21:42:34 +0900

    thymeleaf literal

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -74,7 +74,12 @@ public class BasicController {
         model.addAttribute("param1", "data1");
         model.addAttribute("param2", "data2");
         return "basic/link";
+    }
 
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
     }
 
     @Data
